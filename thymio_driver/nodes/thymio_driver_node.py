a62d315faa861a0e8f9cbe4703de528b12e9e2a2
AnonymSubmit <xy@z.com>
2018-03-04 19:46:54 +0100

    Added tf prefix. Added wheel joint state publisher. Added parameters.

--- thymio_driver/nodes/thymio_driver_node.py
+++ thymio_driver/nodes/thymio_driver_node.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 
+import os
 from math import cos, log, sin
 
 import roslib
 import rospy
 import std_srvs.srv
-
 from asebaros_msgs.msg import AsebaEvent
 from asebaros_msgs.srv import GetNodeList, LoadScripts
 from geometry_msgs.msg import Quaternion, Twist
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Imu, Joy, LaserScan, Range, Temperature
-from std_msgs.msg import Bool, ColorRGBA, Empty, Float32, Int16, Int8
+from sensor_msgs.msg import Imu, JointState, Joy, LaserScan, Range, Temperature
+from std_msgs.msg import Bool, ColorRGBA, Empty, Float32, Int8, Int16
 from tf.broadcaster import TransformBroadcaster
 from thymio_msgs.msg import Led, LedGesture, Sound, SystemSound
 
-BASE_WIDTH = 95     # millimeters
-MAX_SPEED = 500     # units
+
+BASE_WIDTH = 95.0     # millimeters
+MAX_SPEED = 500.0     # units
 SPEED_COEF = 2.93   # 1mm/sec corresponds to X units of real thymio speed
+WHEEL_RADIUS = 22.0   # millimeters
 
 BUTTONS = ['backward', 'forward', 'center', 'right', 'left']
 PROXIMITY_NAMES = ['left', 'center_left', 'center',
@@ -28,18 +30,15 @@ LED_NUMBER = {Led.CIRCLE: 8, Led.PROXIMITY: 8, Led.GROUND: 2,
               Led.REMOTE: 1, Led.BUTTONS: 4, Led.TEMPERATURE: 2, Led.MICROPHONE: 1}
 
 
-class ThymioDriver():
-    # ======== class initialization ========
+class ThymioDriver(object):
+
+    def frame_name(self, name):
+        if self.tf_prefix:
+            return '{self.tf_prefix}/{name}'.format(**locals())
+        return name
 
     def __init__(self):
         rospy.init_node('thymio')
-        # initialize parameters
-        self.x = 0
-        self.y = 0
-        self.th = 0
-        self.then = rospy.Time.now()
-        self.odom = Odometry(header=rospy.Header(
-            frame_id='odom'), child_frame_id='base_link')
 
         # load script on the Thymio
 
@@ -55,59 +54,96 @@ class ThymioDriver():
 
         rospy.wait_for_service('aseba/load_script')
         load_script = rospy.ServiceProxy('aseba/load_script', LoadScripts)
-        script_filename = roslib.packages.get_pkg_dir(
-            'thymio_driver') + '/aseba/thymio_ros.aesl'
+        default_script_path = os.path.join(
+            roslib.packages.get_pkg_dir('thymio_driver'), 'aseba/thymio_ros.aesl')
 
-        load_script(script_filename)
+        script_path = rospy.get_param('~script', default_script_path)
+
+        rospy.loginfo("Load aseba script %s", script_path)
+
+        load_script(script_path)
+
+        # initialize parameters
+
+        self.tf_prefix = rospy.get_param('tf_prefix', '')
+
+        self.odom_frame = self.frame_name(rospy.get_param('~odom_frame', 'odom'))
+        self.robot_frame = self.frame_name(rospy.get_param('~robot_frame', 'base_link'))
+
+        self.x = 0
+        self.y = 0
+        self.th = 0
+        self.then = rospy.Time.now()
+        self.odom_msg = Odometry(header=rospy.Header(frame_id=self.odom_frame),
+                                 child_frame_id=self.robot_frame)
 
         # subscribe to topics
 
         self.aseba_pub = rospy.Publisher(
             'aseba/events/set_speed', AsebaEvent, queue_size=1)
+
+        self.left_wheel_angle = 0
+        self.right_wheel_angle = 0
+
+        self.ticks2mm = rospy.get_param('~ticks2mm', 1.0 / SPEED_COEF)
+        left_wheel_joint = rospy.get_param('~left_wheel_joint', 'left_wheel_joint')
+        right_wheel_joint = rospy.get_param('~right_wheel_joint', 'right_wheel_joint')
+
+        self.wheel_state_msg = JointState()
+        self.wheel_state_msg.name = [left_wheel_joint, right_wheel_joint]
+
+        self.wheel_state_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
         self.odom_pub = rospy.Publisher('odom', Odometry, queue_size=1)
         self.odom_broadcaster = TransformBroadcaster()
-        rospy.Subscriber('aseba/events/odometry', AsebaEvent,
-                         self.on_aseba_odometry_event)
+
+        rospy.Subscriber('aseba/events/odometry', AsebaEvent, self.on_aseba_odometry_event)
         rospy.Subscriber("cmd_vel", Twist, self.on_cmd_vel)
 
         self.buttons = Joy()
         self.buttons_pub = rospy.Publisher('buttons', Joy, queue_size=1)
-        rospy.Subscriber("aseba/events/buttons", AsebaEvent,
-                         self.on_aseba_buttons_event)
+        rospy.Subscriber("aseba/events/buttons", AsebaEvent, self.on_aseba_buttons_event)
 
         for button in BUTTONS:
             rospy.Subscriber('aseba/events/button_' + button,
                              AsebaEvent, self.on_aseba_button_event(button))
 
+        proximity_range_min = rospy.get_param('~proximity/range_min', 0.0215)
+        proximity_range_max = rospy.get_param('~proximity/range_min', 0.14)
+        proximity_field_of_view = rospy.get_param('~proximity/field_of_view', 0.3)
+
         self.proximity_sensors = [{
             'publisher': rospy.Publisher('proximity/' + name, Range, queue_size=1),
-            'msg': Range(header=rospy.Header(frame_id='proximity_' + name + "_link"),
-                         radiation_type=Range.INFRARED,
-                         field_of_view=0.3,
-                         min_range=0.0215,
-                         max_range=0.14)} for name in PROXIMITY_NAMES]
+            'msg': Range(
+                header=rospy.Header(
+                    frame_id=self.frame_name('proximity_{name}_link'.format(name=name))),
+                radiation_type=Range.INFRARED,
+                field_of_view=proximity_field_of_view,
+                min_range=proximity_range_min,
+                max_range=proximity_range_max)}
+            for name in PROXIMITY_NAMES]
 
         self.proximityToLaserPublisher = rospy.Publisher(
             'proximity/laser', LaserScan, queue_size=1)
-        self.proximityToLaser = LaserScan(header=rospy.Header(frame_id="base_link"),
-                                          angle_min=-0.64, angle_max=0.64, angle_increment=0.32,
-                                          time_increment=0, scan_time=0, range_min=0.0215 + 0.08,
-                                          range_max=0.14 + 0.08)
-        rospy.Subscriber('aseba/events/proximity', AsebaEvent,
-                         self.on_aseba_proximity_event)
+        self.proximityToLaser = LaserScan(
+            header=rospy.Header(frame_id=self.frame_name('laser_link')),
+            angle_min=-0.64, angle_max=0.64, angle_increment=0.32,
+            time_increment=0, scan_time=0, range_min=proximity_range_min + 0.08,
+            range_max=proximity_range_max + 0.08)
+        rospy.Subscriber('aseba/events/proximity', AsebaEvent, self.on_aseba_proximity_event)
 
         self.ground_sensors = [{
             'publisher': rospy.Publisher('ground/' + name, Range, queue_size=1),
-            'msg': Range(header=rospy.Header(frame_id='ground_' + name + "_link"),
-                         radiation_type=Range.INFRARED, field_of_view=0.3,
-                         min_range=0.008, max_range=0.008)
+            'msg': Range(
+                header=rospy.Header(
+                    frame_id=self.frame_name('ground_{name}_link'.format(name=name))),
+                radiation_type=Range.INFRARED, field_of_view=proximity_field_of_view,
+                min_range=0.008, max_range=0.008)
         } for name in GROUND_NAMES]
 
-        rospy.Subscriber('aseba/events/ground', AsebaEvent,
-                         self.on_aseba_ground_event)
+        rospy.Subscriber('aseba/events/ground', AsebaEvent, self.on_aseba_ground_event)
         rospy.set_param("~ground_threshold", 200)
 
-        self.imu = Imu(header=rospy.Header(frame_id='base_link'))
+        self.imu = Imu(header=rospy.Header(frame_id=self.robot_frame))
         # no orientation or angular velocity information
         self.imu.orientation_covariance[0] = -1
         self.imu.angular_velocity_covariance[0] = -1
@@ -121,41 +157,33 @@ class ThymioDriver():
                          AsebaEvent, self.on_aseba_accelerometer_event)
 
         self.tap_publisher = rospy.Publisher('tap', Empty, queue_size=1)
-        rospy.Subscriber('aseba/events/tap', AsebaEvent,
-                         self.on_aseba_tap_event)
+        rospy.Subscriber('aseba/events/tap', AsebaEvent, self.on_aseba_tap_event)
 
         self.temperature = Temperature(
-            header=rospy.Header(frame_id='base_link'))
+            header=rospy.Header(frame_id=self.robot_frame))
         self.temperature.variance = 0.01
-        self.temperature_publisher = rospy.Publisher(
-            'temperature', Temperature, queue_size=1)
-        rospy.Subscriber('aseba/events/temperature', AsebaEvent,
-                         self.on_aseba_temperature_event)
+        self.temperature_publisher = rospy.Publisher('temperature', Temperature, queue_size=1)
+        rospy.Subscriber('aseba/events/temperature', AsebaEvent, self.on_aseba_temperature_event)
 
         self.sound_publisher = rospy.Publisher('sound', Float32, queue_size=1)
         self.sound_threshold_publisher = rospy.Publisher(
             'aseba/events/set_sound_threshold', AsebaEvent, queue_size=1)
-        rospy.Subscriber('aseba/events/sound', AsebaEvent,
-                         self.on_aseba_sound_event)
+        rospy.Subscriber('aseba/events/sound', AsebaEvent, self.on_aseba_sound_event)
         rospy.Subscriber('sound_threshold', Float32, self.on_sound_threshold)
 
         self.remote_publisher = rospy.Publisher('remote', Int8, queue_size=1)
-        rospy.Subscriber('aseba/events/remote', AsebaEvent,
-                         self.on_aseba_remote_event)
+        rospy.Subscriber('aseba/events/remote', AsebaEvent, self.on_aseba_remote_event)
 
         rospy.Subscriber('comm/transmit', Int16, self.on_sound_threshold)
-        self.comm_publisher = rospy.Publisher(
-            'comm/receive', Int16, queue_size=1)
+        self.comm_publisher = rospy.Publisher('comm/receive', Int16, queue_size=1)
         self.aseba_set_comm_publisher = rospy.Publisher(
             'aseba/events/set_comm', AsebaEvent, queue_size=1)
-        rospy.Subscriber('aseba/events/comm', AsebaEvent,
-                         self.on_aseba_comm_event)
+        rospy.Subscriber('aseba/events/comm', AsebaEvent, self.on_aseba_comm_event)
 
         # actuators
 
         for name in BODY_LEDS:
-            rospy.Subscriber('led/body/' + name, ColorRGBA,
-                             self.on_body_led(name))
+            rospy.Subscriber('led/body/' + name, ColorRGBA, self.on_body_led(name))
 
         rospy.Subscriber('led', Led, self.on_led)
         self.aseba_led_publisher = rospy.Publisher(
@@ -166,19 +194,16 @@ class ThymioDriver():
         rospy.Subscriber('led/gesture', LedGesture, self.on_led_gesture)
         self.aseba_led_gesture_publisher = rospy.Publisher(
             'aseba/events/set_led_gesture', AsebaEvent, queue_size=6)
-        rospy.Subscriber('led/gesture/circle', Float32,
-                         self.on_led_gesture_circle)
+        rospy.Subscriber('led/gesture/circle', Float32, self.on_led_gesture_circle)
         rospy.Subscriber('led/gesture/off', Empty, self.on_led_gesture_off)
-        rospy.Subscriber('led/gesture/blink', Float32,
-                         self.on_led_gesture_blink)
+        rospy.Subscriber('led/gesture/blink', Float32, self.on_led_gesture_blink)
         rospy.Subscriber('led/gesture/kit', Float32, self.on_led_gesture_kit)
         rospy.Subscriber('led/gesture/alive', Empty, self.on_led_gesture_alive)
 
         rospy.Subscriber('sound/play', Sound, self.on_sound_play)
         self.aseba_led_gesture_publisher = rospy.Publisher(
             'aseba/events/set_led_gesture', AsebaEvent, queue_size=6)
-        rospy.Subscriber('sound/play/system', SystemSound,
-                         self.on_system_sound_play)
+        rospy.Subscriber('sound/play/system', SystemSound, self.on_system_sound_play)
         self.aseba_play_sound_publisher = rospy.Publisher(
             'aseba/events/play_sound', AsebaEvent, queue_size=1)
         self.aseba_play_system_sound_publisher = rospy.Publisher(
@@ -190,6 +215,8 @@ class ThymioDriver():
         rospy.Subscriber('shutdown', Empty, self.on_shutdown_msg)
         self.aseba_shutdown_publisher = rospy.Publisher(
             'aseba/events/shutdown', AsebaEvent, queue_size=1)
+
+        rospy.on_shutdown(self.shutdown)
 
         # tell ros that we are ready
         rospy.Service('thymio_is_ready', std_srvs.srv.Empty, self.ready)
@@ -353,8 +380,8 @@ class ThymioDriver():
                 dist = 0.14
             if dist < 0.0215:
                 dist = 0.0215
-        self.proximityToLaser.ranges.append(dist + 0.08)
-        self.proximityToLaser.intensities.append(raw)
+            self.proximityToLaser.ranges.append(dist + 0.08)
+            self.proximityToLaser.intensities.append(raw)
         self.proximityToLaserPublisher.publish(self.proximityToLaser)
 
     def on_aseba_button_event(self, button):
@@ -383,8 +410,25 @@ class ThymioDriver():
         now = data.stamp
         dt = (now - self.then).to_sec()
         self.then = now
-        dsl = (data.data[0] * dt) / SPEED_COEF  # left wheel delta in mm
-        dsr = (data.data[1] * dt) / SPEED_COEF  # right wheel delta in mm
+
+        vl = data.data[0] * self.ticks2mm
+        vr = data.data[1] * self.ticks2mm
+
+        # wheel joint states
+        left_wheel_angular_speed = vl / WHEEL_RADIUS
+        right_wheel_angular_speed = vr / WHEEL_RADIUS
+
+        self.left_wheel_angle += dt * left_wheel_angular_speed
+        self.right_wheel_angle += dt * right_wheel_angular_speed
+
+        self.wheel_state_msg.header.stamp = rospy.Time.now()
+        self.wheel_state_msg.position = [self.left_wheel_angle, self.right_wheel_angle]
+        self.wheel_state_msg.velocity = [left_wheel_angular_speed, right_wheel_angular_speed]
+        self.wheel_state_pub.publish(self.wheel_state_msg)
+
+        dsl = vl * dt  # left wheel delta in mm
+        dsr = vr * dt  # right wheel delta in mm
+
         # robot traveled distance in meters
         ds = ((dsl + dsr) / 2.0) / 1000.0
         dth = (dsr - dsl) / BASE_WIDTH  # turn angle
@@ -399,29 +443,29 @@ class ThymioDriver():
         quaternion.w = cos(self.th / 2.0)
 
         # prepare odometry
-        self.odom.header.stamp = rospy.Time.now()  # OR TO TAKE ONE FROM THE EVENT?
-        self.odom.pose.pose.position.x = self.x
-        self.odom.pose.pose.position.y = self.y
-        self.odom.pose.pose.position.z = 0
-        self.odom.pose.pose.orientation = quaternion
+        self.odom_msg.header.stamp = rospy.Time.now()  # OR TO TAKE ONE FROM THE EVENT?
+        self.odom_msg.pose.pose.position.x = self.x
+        self.odom_msg.pose.pose.position.y = self.y
+        self.odom_msg.pose.pose.position.z = 0
+        self.odom_msg.pose.pose.orientation = quaternion
 
         if(dt > 0):
-            self.odom.twist.twist.linear.x = ds / dt
-            self.odom.twist.twist.angular.z = dth / dt
+            self.odom_msg.twist.twist.linear.x = ds / dt
+            self.odom_msg.twist.twist.angular.z = dth / dt
 
         # publish odometry
         self.odom_broadcaster.sendTransform(
             (self.x, self.y, 0),
             (quaternion.x, quaternion.y, quaternion.z, quaternion.w),
-            self.then, "base_link", "odom")
-        self.odom_pub.publish(self.odom)
+            self.then, self.robot_frame, self.odom_frame)
+        self.odom_pub.publish(self.odom_msg)
 
     # ======== processing events received from the robot  ========
     def on_cmd_vel(self, data):
         x = data.linear.x * 1000.0  # from meters to millimeters
-        x = x * SPEED_COEF  # to thymio units
+        x = x / self.ticks2mm  # to thymio units
         th = data.angular.z * (BASE_WIDTH / 2)  # in mm
-        th = th * SPEED_COEF  # in thymio units
+        th = th / self.ticks2mm  # in thymio units
         k = max(abs(x - th), abs(x + th))
         # sending commands higher than max speed will fail
         if k > MAX_SPEED:
@@ -429,20 +473,7 @@ class ThymioDriver():
             th = th * MAX_SPEED / k
         self.set_speed([int(x - th), int(x + th)])
 
-    # ======== ======== ======== ======== ======== ======== ========
-    def control_loop(self):
-        rospy.on_shutdown(self.shutdown)  # on shutdown hook
-        while not rospy.is_shutdown():
-            rospy.spin()
-
-
-def main():
-    try:
-        robot = ThymioDriver()
-        robot.control_loop()
-    except rospy.ROSInterruptException:
-        pass
-
 
 if __name__ == '__main__':
-    main()
+    ThymioDriver()
+    rospy.spin()
